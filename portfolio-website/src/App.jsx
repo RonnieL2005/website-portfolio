9ba2e1ff1083ad68e4b9353477014b80d7b47fb9
Ronnie <ronnielangat2020@gmail.com>
2024-04-30 09:58:54 +0300

    added react, new public folder,src folder and installed tailwind css

---
+++ portfolio-website/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { About, Contact, Experience, feedbacks, Hero, Navbar, Tech, Works,
+StarsCanvas } from './components';
+
+function App() {
+  return (
+    < BrowserRouter>
+      <div>
+        this is a 3d developer
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
