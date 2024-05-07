e8e000309aab8945a44d0cd72ae3410c0a427412
Ronnie <ronnielangat2020@gmail.com>
2024-05-07 12:46:57 +0300

    made changes to make the navbar responsive

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,32 +1,42 @@
-import React from "react";
-import './Navbar.css'
+import React, { useState } from "react";
+import './Navbar.css';
 
 function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <nav className="navbar">
-            <div className="navbar-brand">
+            <div className={`navbar-for-Ronnie-dev ${isMenuOpen ? 'show' : ''}`}>
                 <a className="bg-primary- navbar-item raleway-bold" href="/">
                     Ronnie.Dev
                 </a>
             </div>
-            <div className="navbar-menu">
+            <div className={`navbar-menu ${isMenuOpen ? 'show' : ''}`}>
                 <div className="navbar-end">
-                    <a className="navbar-item raleway-bold" href="/">
+                    <a className="navbar-item raleway-bold" href="/About">
                         About
                     </a>
-                    <a className="navbar-item raleway-bold" href="/about">
+                    <a className="navbar-item raleway-bold" href="/Experience">
                         Experience
                     </a>
-                    <a className="navbar-item raleway-bold" href="/contact">
+                    <a className="navbar-item raleway-bold" href="/Projects">
                         Projects
                     </a>
-                    <a className="navbar-item raleway-bold" href="/">
+                    <a className="navbar-item raleway-bold" href="/contact">
                         Contact
                     </a>
                 </div>
             </div>
+            <button className="menu-button" onClick={toggleMenu}>
+                {isMenuOpen ? 'Close Menu' : 'Open Menu'}
+            </button>
         </nav>
     );
 }
 
 export default Navbar;
+
